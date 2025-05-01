2d29aa57c6f878f483938d0b81077516ebba4d5e
Soheil <iamsoheil.r@gmail.com>
2025-05-01 22:02:04 +0330

    Add display movements function

--- script.js
+++ script.js
@@ -61,6 +61,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type} deposit</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+      `;
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
