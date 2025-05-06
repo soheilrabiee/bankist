0ea4f20a602b1d6d51c479d929230e9e1ecd71b2
Soheil <iamsoheil.r@gmail.com>
2025-05-06 18:59:18 +0330

    Add tabbed component functionality

--- marketing-website/script.js
+++ marketing-website/script.js
@@ -92,3 +92,22 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${e.target.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
